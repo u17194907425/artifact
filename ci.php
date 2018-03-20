2782cbcf487441ec51e5d62faf9e41433ca32a25
ggroot <37127098+GGS-WIKI@users.noreply.github.com>
2018-03-20 15:16:44 -0500

    Update ci.php

--- ci.php
+++ ci.php
@@ -29,17 +29,31 @@ $test_card_format = function($f_name) {
 
   //----------------------------------------------------------------------------
   // Card Image
-  // https://regex101.com/r/CM2QKy/4
-  static $card_img_pattern = "~^\!\[[^\]]+\]\(https://(?P<url>[^\)]+)\).*~m";
+  // https://regex101.com/r/CM2QKy/5
+  static $card_img_pattern = "~^\!\[[^\]]+\]\(https://(?P<url>[^\)]+)\)~m";
   validate_using_regexp('Card image', $card_img_md, $card_img_pattern, $f_name, $matches, $exit_code);
 
 
 
   //----------------------------------------------------------------------------
   // Card Type
-  // https://regex101.com/r/teJzXr/1
-  static $card_type_pattern = "~^\*\s+Type: (?P<type>Hero|Creature|Item|Spell)$~m";
+  // https://regex101.com/r/teJzXr/3
+  static $card_type_pattern = "~^\*\s+Type: (?P<type>Hero|Creature|Item|Spell)~m";
   validate_using_regexp('Card type', $card_descr_md, $card_type_pattern, $f_name, $matches, $exit_code);
+  $card_type = $matches[0]['type'];
+
+
+  //----------------------------------------------------------------------------
+  // Card Color
+  // https://regex101.com/r/HXNxNU/3
+  static $card_color_pattern = "~^\*\s+Color: (?P<color>Red|Green|Blue|Black|None|\*\*Unknown\*\*)~m";
+  $expected_matches = 1;
+  if ($card_type == 'Item') {
+    $expected_matches = 0;
+  }
+  validate_using_regexp('Card color', $card_descr_md, $card_color_pattern, $f_name, $matches, $exit_code, $expected_matches);
+
+
 };
 
 
@@ -76,16 +90,19 @@ function apply_check_on_dir($path = "", $fun) {
 
 
 
-function validate_using_regexp($descr = '', $str = '', $pattern = '~.*~i', $f_name, &$matches, &$exit_code) {
+function validate_using_regexp($descr = '', $str = '', $pattern = '~.*~i', $f_name, &$matches, &$exit_code, $expected_matches = 1) {
   if (!preg_match_all($pattern, $str, $matches, PREG_SET_ORDER)) {
-    $exit_code = 1;
-    print "[ERROR]: $descr is not properly formatted in ($f_name)\n";
-    return;
+    if ($expected_matches != 0) {
+      $exit_code = 1;
+      print "[ERROR]: $descr is not properly formatted in ($f_name)\n";
+      return;
+    }
   }
 
-  if (count($matches) > 1) {
+  if (count($matches) != $expected_matches) {
     $exit_code = 1;
-    print "[ERROR]: more than a single $descr found in ($f_name)\n";
+    print "[ERROR]: $descr is found " . count($matches) . " times but expected "
+      . $expected_matches . " times in ($f_name)\n";
     return;
   }
 }
